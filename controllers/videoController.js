697ecdd2595357226885472bee71efc4e1e3bdff
Amal K K <kkamalkk1994@gmail.com>
2022-02-16 20:44:48 +0530

    API results stored to file to avoid quota limit during testing

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const superagent = require("superagent");
+const fs = require("fs");
 
 const app = express();
 
 exports.getAllVideos = async (req, res) => {
   try {
-    const response = await superagent.get(
-      `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY}&channelId=${process.env.CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`
-    );
+    let resp = "";
+    if (process.env.MODE === "OFFLINE") {
+      console.log("offline mode...");
+      const offlineData = fs.readFileSync(
+        `${__dirname}/../data/youtubeAPIResultSmall.json`,
+        "utf-8"
+      );
+      resp = JSON.parse(offlineData);
+    } else {
+      const response = await superagent.get(
+        `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY}&channelId=${process.env.CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`
+      );
+      resp = JSON.parse(response.text);
+    }
     //   console.log(response);
-    const resp = JSON.parse(response.text);
     //   console.log("resp", resp);
     res.status(200).json(resp);
   } catch (err) {
